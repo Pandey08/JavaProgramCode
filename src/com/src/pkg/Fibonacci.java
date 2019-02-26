2613a3d465559d2261ea70afd913e6b80598f609
Bishal Pandey <bishalisonline@gmail.com>
2019-02-26 17:37:46 +0530

    Adding Fibonacci.java to Repo

---
+++ src/com/src/pkg/Fibonacci.java
@@ -0,0 +1,26 @@
+package com.src.pkg;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner inp=new Scanner(System.in);
+		System.out.println("Enter the count:  ");
+		int count=inp.nextInt();
+		int n1=0,n2=1,n3,i;
+		System.out.print(n1+"  "+n2);
+		
+		for(i=2;i<count;++i)
+		{
+			n3=n1+n2;
+			System.out.print("  "+n3);
+			n1=n2;
+			n2=n3;
+		}
+
+	}
+
+}
