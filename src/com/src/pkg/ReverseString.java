fb614196785c2303c1523ba404b5ed24dd0b2ab9
Bishal Pandey <bishalisonline@gmail.com>
2019-02-28 17:57:08 +0530

    Modifying the Reverse Program

--- src/com/src/pkg/ReverseString.java
+++ src/com/src/pkg/ReverseString.java
@@ -19,13 +19,28 @@ public class ReverseString {
 		System.out.println("Enter the second string:  " );
 		String s=inp.nextLine();
 		String rev="";
-		int len=s.length();
-		for(int i=len-1;i>0;i--)
-		{
-			rev=rev+s.charAt(i);
-		}
-		System.out.println(rev);
-	}
+		//int len=s.length();
+		//for(int i=len-1;i>0;i--)
+		//{
+		//	rev=rev+s.charAt(i);
+		//}
+		//System.out.println(rev);
+		StringBuffer sf=new StringBuffer(s);
+		System.out.println(sf.reverse());
+		/*How to Reverse a String:
+1. using for loop and charAt() method
+2. using StringBuffer class and reverse function
+
+Interview Questions:
+//Reverse a String
+//Difference between String and StringBuffer
+//Do we have reverse function in String?
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+	}  
 	
 
 }
